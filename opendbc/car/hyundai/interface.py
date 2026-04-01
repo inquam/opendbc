eaa6061774d202f7c4c9201d60fe6435f9511e24
Daniel Liljeberg <damien_@hotmail.com>
2026-04-01 17:45:11 +0200

    Disable dashcamOnly for KIA_OPTIMA_H
    Comment out dashcamOnly assignment for KIA_OPTIMA_H.

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -149,8 +149,8 @@ class CarInterface(CarInterfaceBase):
 
     # Dashcam cars are missing a test route, or otherwise need validation
     # TODO: Optima Hybrid 2017 uses a different SCC12 checksum
-    if candidate in (CAR.KIA_OPTIMA_H,):
-      ret.dashcamOnly = True
+    #if candidate in (CAR.KIA_OPTIMA_H,):
+    #  ret.dashcamOnly = True
 
     return ret
 
